775e1ec486ee2ef19f8667f71e9f903e8a4f70c0
Robbin <robbin.tapper@djakne.com>
2025-12-20 18:18:22 +0100

    Fix camera issue

--- custom_components/ev_smart_charger/logbook.py
+++ custom_components/ev_smart_charger/logbook.py
@@ -1,18 +1,20 @@
 """Describe logbook events."""
+
 from homeassistant.core import callback
-from homeassistant.const import ATTR_NAME, ATTR_MESSAGE
+from homeassistant.const import ATTR_NAME
 from .const import DOMAIN
+
 
 @callback
 def async_describe_events(hass, async_describe_event):
     """Describe logbook events."""
-    
+
     @callback
     def async_describe_logbook_event(event):
         """Describe a logbook event."""
         return {
             "name": event.data.get(ATTR_NAME, "EV Smart Charger"),
-            "message": event.data.get(ATTR_MESSAGE),
+            "message": event.data.get("message"),
         }
 
     async_describe_event(DOMAIN, f"{DOMAIN}_log_event", async_describe_logbook_event)
